f49f4296d3a9a801371fe912dcaf2938ba94f9eb
arminkardan <armin.fire@gmail.com>
2022-09-18 10:16:46 +0430

    Update

---
+++ run.js
@@ -0,0 +1,29 @@
+const venom = require('venom-bot');
+
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: true // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+async function start(client) {
+
+// console.log(await client.getAllChatsContacts())
+
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Venom 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
